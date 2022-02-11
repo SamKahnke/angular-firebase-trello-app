047c70ba6d37084b441be16087f54333e7d90fbe
SamKahnke <samkahnke@gmail.com>
2022-02-11 16:25:47 -0600

    create shell with nav. make responsive for handheld

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 
-
 @NgModule({
   declarations: [
     AppComponent
